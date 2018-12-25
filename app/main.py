90bc1becf93b3c2e9c59a911113e7f316e86dbad
m3xan1k <job.shevtsov@ya.ru>
2018-12-25 10:22:28 +0300

    add iteration through pages

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import os.path
 
 url = 'https://avito.ru/'
 params = {
@@ -10,14 +11,44 @@ params = {
     'query': 'бультерьер'
 }
 
-full_query = url + params['city'] + '?q=' + params['query']
+pages = [i for i in range(6) if i > 0]
 
-r = requests.get(full_query)
-html = r.text
+def construct_query(url, page, params):
+    full_query = '{0}{1}?p={2}q={3}'.format(url, params['city'], page, params['query'])
+    return full_query
 
-soup = BeautifulSoup(r.text, 'lxml')
+def get_html(full_query):
+    r = requests.get(full_query)
+    html = r.text
+    return html
 
-print(soup)
+def read_pages(pages):
+    for page in pages:
+        full_query = construct_query(url, page, params)
+        html = get_html(full_query)
+        if os.path.exists('bully.html'):
+            with open('bully.html', 'a') as f:
+                f.write(html)
+        else:
+            with open('bully.html', 'w') as f:
+                f.write(html)
+
+
+
+
+
+# r = requests.get(full_query)
+# html = r.text
+#
+# soup = BeautifulSoup(r.text, 'lxml')
+#
+# print(soup)
 
 # with open('output.txt', 'w') as f:
 #     f.write(r.text)
+
+
+def main():
+    read_pages(pages)
+
+main()
